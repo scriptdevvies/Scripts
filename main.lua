5ecabf8153fabaefc9cb5a308c1329ada3a36939
scriptdevvies <scriptdevvies@gmail.com>
2024-10-21 19:59:36 -0500

    v1: added base functions

---
+++ main.lua
@@ -0,0 +1,73 @@
+-- Scripts provided
+local scripts = {
+  ["prizzlife"] = "https://raw.githubusercontent.com/elliexmln/PrizzLife/main/pladmin.lua"
+}
+
+-- Script initialization
+local module = {}
+
+-- Functions (help)
+local function module:GetScriptList()
+  return scripts
+end
+
+-- Functions (Scripts)
+local function module:LoadScript(scr:string)
+  if scripts[scr] then
+    loadstring(game:HttpGet((scripts[scr]),true))()
+  else
+    return error("Invalid script, use the GetScriptList() function")
+  end
+end
+
+-- Functions (Tools)
+-- loadstring() via HttpGet (put into a function)
+local function module.tools:LoadStringHttp(url:string)
+  if url then
+    loadstring(game:HttpGet((url),true))()
+  else
+    return error("No URL specified")
+  end
+end
+
+-- loadstring() via string (bytecode or minified, no HttpGet)
+local function module.tools:LoadString(str:string)
+  if str then
+    loadstring(str)
+  else
+    return error("Please specify a string")
+  end
+end
+
+
+-- Create loadstring() function via variable that can be activated
+local function module.tools:CreateLoadString(url:string,str:string)
+  if url then
+    return module.core:__createloadstring(url)
+  else
+    return module.core:__createloadstring(str)
+  end
+end
+
+local function module.core:__createloadstring(url:string,str:string)
+  local __func = {}
+  if url then
+    local function __func:Activate()
+      loadstring(game:HttpGet((url),true)()
+    end
+  elseif str then
+    local function __func:Activate()
+      loadstring(str)
+    end
+  else
+    local function __func:Activate()
+      if __func.str then
+        loadstring(__func.str)
+      else
+        return error("No string to activate, set func.str to the string you want to load (func is loadstring created and returned)")
+      end
+  end
+return __func
+end
+
+return module
